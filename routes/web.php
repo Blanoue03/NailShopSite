0a267d163bc897ca30296d64488b195ac4f2cdf7
ConnorLogan-Odd-likeArchives <W0874330@myscc.ca>
2026-03-21 16:41:25 -0400

    Created the resource route method thing for you guys to use, if we like to simplify things just like what we did to the articles in a PHP assignment we did not too long ago.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('pages.home');
-});
+    return Inertia::render('Home');
+})->name('home');
+
+Route::resource('store', StoreController::class);
